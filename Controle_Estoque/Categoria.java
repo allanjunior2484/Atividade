411f781ff7888953767d486c27376de3e90c3239
Allan Junior Batista Silva <allanjunior993@gmail.com>
2024-10-22 08:14:33 -0300

    Update Categoria.java

--- Controle_Estoque/Categoria.java
+++ Controle_Estoque/Categoria.java
@@ -1,4 +1,24 @@
-public class Categoria{
-	String NomeCategoria;
-	Produto produto;
+public class Categoria {
+   private String nomeCategoria;
+   // construtores
+
+    // Construtor  define um nome (Categoria não defiida)
+    public Categoria() {
+        this.nomeCategoria = "Categoria Não definida"; // Define um nome padrão
+    }
+    // define que nomeCategoria ira receber String
+    public Categoria (String nomeCategoria){
+        this.nomeCategoria = nomeCategoria; // Inicializa o atributo
+    }
+    //  metodo get para obter nome categoria
+    public String getNomeCategoria(){
+        return nomeCategoria; // Retorna o valor do atributo
+    }
+    //Metodo set para modificar o nome da categoria
+
+     public void setNomeCategoria(String nomeCategoria){
+
+        this.nomeCategoria = nomeCategoria; // Define um novo valor para o atributo
+    }
+
 }
