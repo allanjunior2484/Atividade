96b6af6fed72ee9eae141ac6b86e4b597801d3d6
Allan Junior Batista Silva <allanjunior993@gmail.com>
2024-10-22 08:18:08 -0300

    Update ServicoReabastecimento.java

--- Controle_Estoque/ServicoReabastecimento.java
+++ Controle_Estoque/ServicoReabastecimento.java
@@ -1,10 +1,41 @@
-public class ServicoReabastecimento{
-	Produto produto;
-	float quantidadeProduto;
+public class ServicoReabastecimento {
+    private Produto produto; // Produto a ser reabastecido
+    private float quantidadeProduto; // Quantidade a ser reabastecida
 
-	public void atualizarQuantidade(float novaQuantidade) {
-        quantidadeProduto = novaQuantidade;
+
+    // Construtor que inicializa apenas o produto, com quantidade padrão de 0
+    public ServicoReabastecimento(Produto produto) {
+        this.produto = produto; // Inicializa o produto
+        this.quantidadeProduto = 0; // Inicializa a quantidade como 0
+    }
+    // Construtor
+    public ServicoReabastecimento(Produto produto, float quantidade) {
+        this.produto = produto; // Inicializa o produto
+        this.quantidadeProduto = quantidade; // Inicializa a quantidade
+    }
+
+    // Metodo para reabastecer o produto
+    public void reabastecer(float quantidade) {
+        quantidadeProduto += quantidade; // Aumenta a quantidade no estoque
+    }
+
+    // Metodo para obter a quantidade atual do produto
+    public float getQuantidadeProduto() {
+        return quantidadeProduto; // Retorna a quantidade atual
+    }
+
+    // Metodo para obter o produto
+    public Produto getProduto() {
+        return produto; // Retorna o produto
+    }
+
+    // Metodo para definir um novo produto
+    public void setProduto(Produto produto) {
+        this.produto = produto; // Atualiza o produto
+    }
+
+    // Metodo para definir uma nova quantidade de produto
+    public void setQuantidadeProduto(float quantidade) {
+        this.quantidadeProduto = quantidade; // Atualiza a quantidade do produto
     }
 }
-
-
